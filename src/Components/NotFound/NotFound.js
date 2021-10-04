3ca51b48ad6ac12f923fdd28bfd3617931c620d4
tanvirnpm <tanvirnpm@gmail.com>
2021-10-04 23:08:01 +0600

    not found page added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Not found section</h1>
+        <div className="container py-5">
+            <div className="row justify-content-center">
+                <div className="col-6">
+                    <img className="img-fluid rounded" src="https://www.pngitem.com/pimgs/m/254-2549834_404-page-not-found-404-not-found-png.png" alt="" srcset="" />
+                </div>
+            </div>
         </div>
     );
 };
